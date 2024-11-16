a96d899e2cdbc34d984240809c7f606025a6c373
pavel123qwe <light.alone@yandex.ru>
2024-11-16 23:41:51 +0300

    123

--- Projectt/shop/users/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.f
-from django.contrib.auth.models import AbstractUser
-class User(AbstractUser):
-	pass
--- Projectt/shop/users/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.urls import path,include
-from django.views.generic import TemplateView
-from .views import *
-
-urlpatterns = [
-    path('',include('django.contrib.auth.urls')),
-	path('register',Register.as_view(),name="register")
-]
